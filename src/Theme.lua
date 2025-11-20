612774514ce18f17edd3d4f5ec1f58f3641fe4aa
TMonster <anhtuantruong423@gmail.com>
2025-11-20 17:31:30 +0700

    Create Theme.lua

---
+++ src/Theme.lua
@@ -0,0 +1,13 @@
+local Theme = {}
+
+Theme.Dark = {
+    Background = Color3.fromRGB(15,15,15),
+    Text = Color3.new(1,1,1)
+}
+
+Theme.Light = {
+    Background = Color3.fromRGB(255,255,255),
+    Text = Color3.new(0,0,0)
+}
+
+return Theme
